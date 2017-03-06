8541bebeddb72cd3dfe1bda5cc31d453584320e7
Marek Valda <MValda@seznam.cz>
2017-03-07 00:18:39 +0100

    fix: 1st working phantomjs script

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,13 +22,13 @@ script1 = { 1 => "goto 'seznam.cz'",
             12 => "element(xpath: '//div[4]/div[2]/div/div/div[4]/div/a[1]').click" }
 
 script2 = { 1 => "goto 'http://pocasi.idnes.cz'",
-            2 => "element(xpath: '//div[5]/div[3]/div/div/div/div[2]/div[1]/div[6]/div/p').select_text browser.element(xpath: '//div[5]/div[3]/div/div/div/div[2]/div[1]/div[6]/div/p').text.lines[0]",
-            3 => "send_keys [:command, 'c']",
+            2 => "div(class: 'art text-forecast').select_text browser.div(class: 'art text-forecast').text.lines[1]",
+            3 => "send_keys [:control, 'c']",
             4 => "goto 'poslatsms.cz'",
             5 => "textarea(id: 'textsms').set 'Předpověd na datum: '",
             6 => "textarea(id: 'textsms').append (Time.zone.now + 1.day).to_date",
             7 => "textarea(id: 'textsms').append ' - '",
-            8 => "textarea(id: 'textsms').send_keys [:command, 'v']",
+            8 => "textarea(id: 'textsms').send_keys [:control, 'v']",
             9 => "text_field(name: 'cislo-prijemce').set '775191055'",
             10 => "element(id: 'odeslat-sms').click",
             11 => "element(id: 'odeslat-sms').wait_while_present" }
@@ -36,7 +36,7 @@ script2 = { 1 => "goto 'http://pocasi.idnes.cz'",
 puts 'CREATING AUTOMATIONS'
 
 automation1 = Automation.create(name: 'Zaslání emailu', active: true, browser_type: 'chrome', user_id: user.id)
-automation2 = Automation.create(name: 'Předpověd počasí', active: true, browser_type: 'firefox', user_id: user.id)
+automation2 = Automation.create(name: 'Předpověd počasí', active: true, browser_type: 'phantomjs', user_id: user.id)
 
 puts 'ADDING PROCEDURES'
 
